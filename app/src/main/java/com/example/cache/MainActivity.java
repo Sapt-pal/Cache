2d139cc84c0fa5d4d7e4a21fc5ac266a1904e213
Sapt-pal <saptarshipal04@gmail.com>
2023-08-05 17:47:03 +0530

    created greet.java, greeting.xml, updated AndroidManifest.xml

--- app/src/main/java/com/example/cache/MainActivity.java
+++ app/src/main/java/com/example/cache/MainActivity.java
@@ -2,20 +2,29 @@ package com.example.cache;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.cache.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void proceed(View view) {
-        Toast.makeText(this, "Proceeding...", Toast.LENGTH_SHORT).show();
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void proceed(View view) {
+        EditText userName = findViewById((R.id.userName));
+        String name = userName.getText().toString();
+
+        //start new activity on proceed and pass on name as intent extra
+        Intent intent = new Intent(this, greet.class);
+        intent.putExtra("username", name);
+        Toast.makeText(this, "Proceeding...", Toast.LENGTH_SHORT).show();
+        startActivity(intent);
     }
 }
